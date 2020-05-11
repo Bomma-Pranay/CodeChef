10a64ccf19c87db81c4fa791c483c04a3ff44004
Bomma-Pranay <62324691+Bomma-Pranay@users.noreply.github.com>
2020-05-11 22:14:29 +0530

    Create README.py

---
+++ May long challenge 2020/README.py
@@ -0,0 +1,5 @@
+This was really a tough competitive long challenge, the qulaity of questions as 
+compared to April long challenge was moderate-to-tough. In Codechef May long challenge 2020 my global rank was 4301. 
+This competition was so special to me, I became a 3 star code on Codechef !!!. In this challenge, I got my best global rank so far.
+I solved 2 full problems ( All Correct ) and 2 partially correct. You can see my solutions in this folder. Any issues or queries will
+be welcomed. Hope this helps :D
