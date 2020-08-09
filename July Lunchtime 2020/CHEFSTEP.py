b638d147a181f8bbdbc5d6f7b4ade17b9f990d20
Bomma-Pranay <62324691+Bomma-Pranay@users.noreply.github.com>
2020-08-09 16:27:23 +0530

    Verdict : AC 100 points

---
+++ July Lunchtime 2020/CHEFSTEP.py
@@ -0,0 +1,22 @@
+# Problem Link : https://www.codechef.com/LTIME86B/problems/CHEFSTEP
+
+# My Solution
+
+# cook your dish here
+# Problem 1
+tc=int(input())
+for xyz in range(tc):
+    l=list(map(int,input().split()))
+    n,k=l[0],l[1]
+    k=k%(1000000001)
+    # s=''
+    li=list(map(int,input().split()))
+    # print(li)
+    # print(s)
+    s=''
+    for i in li:
+        if (i%1000000001)%(k)==0:
+            s=s+'1'
+        else:
+            s=s+'0'
+    print(s)
