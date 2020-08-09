91bb39064e22bed84de180a4fb75f9c7cbf42f36
Bomma-Pranay <62324691+Bomma-Pranay@users.noreply.github.com>
2020-08-09 16:05:53 +0530

    My Python Solution ( AC 100 points )

---
+++ May Lunchtime 2020/LOSTWKND.py
@@ -0,0 +1,14 @@
+#Problem Link : https://www.codechef.com/LTIME84B/problems/LOSTWKND
+
+#My Solution : 
+
+tc=int(input())
+for xyz in range(tc):
+    l=list(map(int,input().split()))
+    p=l.pop()
+    for i in range(len(l)):
+        l[i]=l[i]*p
+    if sum(l)<=120:
+        print('No')
+    else:
+        print('Yes')
